62515c95c829f8084cfa77da2c287634f115a273
marcotornese <tornesemarco@gmail.com>
2024-11-26 02:17:48 -0300

    enrutamiento
    enrutado de app.routes para poder dirigir a los diferentes paths

--- digesto-frontend/src/app/app.routes.ts
+++ digesto-frontend/src/app/app.routes.ts
@@ -1,3 +1,50 @@
-import { Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { NgModel } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CompoUsuarioLoginComponent } from './componentes/compo-usuario-login/compo-usuario-login.component';
+import { CompoCaracteristicaComponent } from './componentes/compo-caracteristica/compo-caracteristica.component';
+import { CompoDocumentoComponent } from './componentes/compo-documento/compo-documento.component';
 
-export const routes: Routes = [];
+
+
+//componente login
+//componente caracteristica
+//componente documento
+
+const routes : Routes = [
+{
+    path: '',
+},
+{
+    path: 'login',
+    component: CompoUsuarioLoginComponent
+},
+{
+    path: 'caracteristica',
+    component: CompoCaracteristicaComponent
+},
+{
+    path: 'documento',
+    component : CompoDocumentoComponent
+}
+]
+
+@NgModule({
+    declarations :[
+        AppComponent,
+        CompoCaracteristicaComponent,
+        CompoDocumentoComponent,
+        CompoUsuarioLoginComponent,
+        CompositionEvent
+        ],
+        imports : [
+            BrowserModule,
+            RouterModule.forRoot(routes)
+        ],
+        providers : [],
+        bootstrap :[AppComponent]
+    
+})
+export class AppModule { }
---
+++ digesto-frontend/src/app/componentes/compo-principal/compo-principal.component.ts
@@ -0,0 +1,13 @@
+
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes } from '@angular/router';
+
+
+@Component({
+    selector: 'app-compo-principal',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './compo-principal.component.html',
+    styleUrl: './compo-principal.component.css'
+    })
